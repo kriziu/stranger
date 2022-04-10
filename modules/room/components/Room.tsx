4ce33fe1e88a0200567dd32b5b25cd0a77b59d01
kriziu <brunodzi07@gmail.com>
2022-04-10 19:58:41 +0200

    Update Room.tsx

--- modules/room/components/Room.tsx
+++ modules/room/components/Room.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 
 import { motion } from 'framer-motion';
-import { BsChevronUp } from 'react-icons/bs';
 import { FaVideo } from 'react-icons/fa';
 import { useBoolean, useMedia } from 'react-use';
 
